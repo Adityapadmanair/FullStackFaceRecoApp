b14c9bafb2e649aa83bc2fe9de7d1a1c8e63d51b
Adityapadmanair <adityapnair15@gmail.com>
2024-03-12 19:51:09 +0100

    Tachyeon dependency and adding logo to app

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ function App() {
   return (
     <div className="App">
       <Navigation />
-      {/*<Logo />
-      <ImageLinkForm />
+      <Logo />
+      {/* <ImageLinkForm />
       <FaceRecognition /> */}
 
 
