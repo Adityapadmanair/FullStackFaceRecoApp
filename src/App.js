44ab3024b684a322212cd7e38e8b03d0b185917b
Adityapadmanair <adityapnair15@gmail.com>
2024-03-21 04:59:06 +0100

    removed errors ig

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, {useCallback} from 'react';
-import Particles from "react-tsparticles";
-import { loadSlim } from "tsparticles-slim";
+import React,{ useCallback, useEffect, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadSlim } from "@tsparticles/slim"; // if you are going to use `loadSlim`, install the "@tsparticles/slim" package too.
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
@@ -9,53 +9,106 @@ import './App.css';
 
 
 
-const App =() =>{
-  const particlesInit = useCallback(async engine => {
-    console.log(engine);
-    // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
-    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-    // starting from v2 you can add only the features you need reducing the bundle size
-    //await loadFull(engine);
-    await loadSlim(engine);
-}, []);
+const App = () => {
+  const [ init, setInit ] = useState(false);
 
-const particlesLoaded = useCallback(async container => {
-    await console.log(container);
-}, []);
+  // this should be run only once per application lifetime
+  useEffect(() => {
+      initParticlesEngine(async (engine) => {
+          // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+          // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+          // starting from v2 you can add only the features you need reducing the bundle size
+          //await loadAll(engine);
+          //await loadFull(engine);
+          await loadSlim(engine);
+          //await loadBasic(engine);
+      }).then(() => {
+          setInit(true);
+      });
+  }, []);
+
+  const particlesLoaded = (container) => {
+      console.log(container);
+  };
 
 
-const options = {
-  // Define particle configuration here
-  fpsLimit: 60,
-  interactivity: {
-    detectsOn: "canvas",
-  },
-  particles: {
-    number: {
-      value: 80,
-    },
-    shape: {
-      type: "circle",
-      stroke: {
-        width: 0,
-        color: "#fff",
-      },
-    },
-    move: {
-      direction: "none",
-      outMode: "out",
-    },
-  },
-};
+
 
   return (
     <div className="App">
       <Particles className= "particles" 
-        id="tsparticles" 
-        url="http://foo.bar/particles.json" 
-        init={particlesInit} 
-        loaded={particlesLoaded} 
-        options={options}/>
+         id="tsparticles"
+         particlesLoaded={particlesLoaded}
+         options={{
+             background: {
+                 color: {
+                     value: "#0d47a1",
+                 },
+             },
+             fpsLimit: 120,
+             interactivity: {
+                 events: {
+                     onClick: {
+                         enable: true,
+                         mode: "push",
+                     },
+                     onHover: {
+                         enable: true,
+                         mode: "repulse",
+                     },
+                     resize: true,
+                 },
+                 modes: {
+                     push: {
+                         quantity: 4,
+                     },
+                     repulse: {
+                         distance: 200,
+                         duration: 0.4,
+                     },
+                 },
+             },
+             particles: {
+                 color: {
+                     value: "#ffffff",
+                 },
+                 links: {
+                     color: "#ffffff",
+                     distance: 150,
+                     enable: true,
+                     opacity: 0.5,
+                     width: 1,
+                 },
+                 move: {
+                     direction: "none",
+                     enable: true,
+                     outModes: {
+                         default: "bounce",
+                     },
+                     random: false,
+                     speed: 6,
+                     straight: false,
+                 },
+                 number: {
+                     density: {
+                         enable: true,
+                         area: 800,
+                     },
+                     value: 80,
+                 },
+                 opacity: {
+                     value: 0.5,
+                 },
+                 shape: {
+                     type: "circle",
+                 },
+                 size: {
+                     value: { min: 1, max: 5 },
+                 },
+             },
+             detectRetina: true,
+         }}
+     />
       <Navigation />
       <Logo />
       <Rank />
